b64c752e204d689dd14c9b022c867bd9ff7363a7
Jennifer Rubinovitz <jrubinovitz@gmail.com>
2013-04-26 19:59:57 -0400

    redundancies removed

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ from flask import Flask, request, Response, render_template
 import datetime
 import logging
 import os
-import random
 import re
-import json
-import jinja2
-from socketio import socketio_manage
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
 import redis 
